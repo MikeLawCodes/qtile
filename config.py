46ebf24dabab6aacc7f783c5bdc5ff2bf412f593
UnhingedNW <mjl93@me.com>
2022-09-13 23:35:08 -0700

    Changed a few minor colors

--- config.py
+++ config.py
@@ -189,7 +189,7 @@ layouts = [
 
 widget_defaults = dict(
     font = "FiraCode Nerd Fonts",
-    fontsize = 12,
+    fontsize = 14,
     padding = 3,
 )
 extension_defaults = widget_defaults.copy()
@@ -209,14 +209,15 @@ screens = [
                     name_transform=lambda name: name.upper(),
                 ),
         #        widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-d&gt; Rofi", foreground="#d75f5f"),
+                widget.TextBox("Press &lt;M-d&gt; Rofi", foreground=catppuccin["Red"]),
                 widget.Systray(),
                 widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
                 widget.QuickExit(),
             ],
-            24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+            32,
+            border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            border_color=catppuccin["Sapphire"]
+            #border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
     ),
 ]
